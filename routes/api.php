14cefb1e858224442353a3ec9e6c33a0ebdd5686
Trick Informatics <shishil.patel@ymail.com>
2018-08-14 22:20:10 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/lists', 'iIMDBController@index')->name('lists');
+
+
+Route::prefix('movie')->group(function () {
+
+    Route::get('/', 'iIMDBController@index');
+    Route::get('/title/{title}', 'iIMDBController@searchByTitle');
+    Route::get('/year/{year}', 'iIMDBController@searchByYear');
+    Route::get('/rating/{rating}', 'iIMDBController@searchByRating');
+    Route::get('/genre/{genre}', 'iIMDBController@searchByGenre');
+
+
+    Route::put('/rating/{title}/{rating}', 'iIMDBController@update');
+    Route::put('/genre/{title}/{genre}', 'iIMDBController@update');
+});
